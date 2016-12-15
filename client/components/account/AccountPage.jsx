f03092d2680b07f074dd1da33e7579b6c9673e9c
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2016-12-15 17:01:12 +0000

    start to look at the form

--- client/components/account/AccountPage.jsx
+++ client/components/account/AccountPage.jsx
@@ -3,8 +3,28 @@ import React from 'react';
 const AccountPage = (props) => {
   console.log(Meteor.userId())
   console.log(props)
+
+    // hideForm() {
+    //   $('.edit').on('click', function() {
+    //     $('[readonly]').attr('readonly', false);
+    //     $('.readonly').removeClass('readonly')
+    //   });
+    // }
+
     return(
       <div>
+        <form action="" className="readonly editForm">
+          <label>First Name: </label>
+          <input type="text" value={props.user.FirstName} readOnly />
+           <label>Last Name: </label>
+          <input type="text" value={props.user.secondName} readOnly />
+              <label>Email: </label>
+          <input type="email" value={props.user.email} readOnly />
+              <label>Website: </label>
+          <input type="text" value="willforsyth.io" readOnly />
+            <input type="submit" />
+            <a href="#" className="edit">Edit form</a>
+        </form>
         <h1>{props.user.firstName}</h1>
         <p>{props.user._id}</p>
       </div>
