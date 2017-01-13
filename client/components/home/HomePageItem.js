e507e446b1764120b8cabf41beebd05b9b80d249
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2017-01-13 15:11:47 +0000

    rework some bits

--- client/components/home/HomePageItem.js
+++ client/components/home/HomePageItem.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {Link, IndexLink} from 'react-router';
 
 const HomePageItem = (props) => (
+
       <article className="column column__3-of-12 item__thumbnail item" style={props.styles.base}>
-        <img src={props.user.thumbnail} title="" />
+        <IndexLink to={"details/" + props.user.title} activeClassName="active">
+          <img src={props.user.thumbnail} title="" />
+        </IndexLink>
         <h2>{props.user.title}</h2>
         <p>{props.user.category}</p>
         <IndexLink to={"details/" + props.user.title} activeClassName="active">View details</IndexLink>
